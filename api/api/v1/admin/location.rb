7b74859bfd0c8cd70c216fe11ac7698b9b71997e
Thanh Pham <ppthanhtn@gmail.com>
2016-10-14 01:22:42 +0700

    [api] add skeleton for location api

---
+++ api/api/v1/admin/location.rb
@@ -0,0 +1,13 @@
+module Townsquare
+  module API
+    class V1 < Grape::API
+      resource 'admin/locations' do
+
+        get '/' do
+          
+        end
+         
+      end
+    end
+  end
+end
