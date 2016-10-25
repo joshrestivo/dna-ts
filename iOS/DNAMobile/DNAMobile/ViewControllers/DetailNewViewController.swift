9257547e3d6d2d5bc19c3ffebbb402841c5916bb
Tam Duong <dctamtn@yahoo.com.vn>
2016-10-25 12:55:19 +0700

    [IOS] improve UI

--- iOS/DNAMobile/DNAMobile/ViewControllers/DetailNewViewController.swift
+++ iOS/DNAMobile/DNAMobile/ViewControllers/DetailNewViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class DetailNewViewController: BaseViewController {
 
+    @IBOutlet weak var btnTitle: UIButton!
     @IBOutlet weak var webview: UIWebView!
+    @IBOutlet weak var btnDataSource: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ConstantHelper.roundButton(btnTitle, color: ConstantHelper.redColor, radius: 5)
+        btnTitle.backgroundColor = ConstantHelper.redColor
         webview.loadRequest(URLRequest(url: URL(string:"http://www.downtownstl.org/downtown-street-alert-38/")!))
     }
 
