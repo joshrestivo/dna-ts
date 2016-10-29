6b1fac961e0f6d803d2b47af05553d5d43367df8
Thanh Pham <ppthanhtn@gmail.com>
2016-10-29 13:52:07 +0700

    [api] update db schema

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026200000) do
+ActiveRecord::Schema.define(version: 20161029200000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20161026200000) do
     t.string   "updated_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
   create_table "devices", id: :bigserial, force: :cascade do |t|
