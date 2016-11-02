035dc43eb096c36a7c5f9a32890577bf8b31a006
Thang <thangdx2901@gmail.com>
2016-11-02 15:38:12 +0700

    [admin-build] Update build.

--- admin-build/js/all.js
+++ admin-build/js/all.js
@@ -115,8 +115,8 @@ if (cookie == null){
 }
 
 // Define global variables
-//SERVICE_BASE_URL = "http://localhost:9002/api/1.0";
-SERVICE_BASE_URL = "https://townsquare-dev.herokuapp.com/api/1.0";
+SERVICE_BASE_URL = "http://localhost:9002/api/1.0";
+//SERVICE_BASE_URL = "https://townsquare-dev.herokuapp.com/api/1.0";
 SERVER_ERROR_MSG = "Server is error. Please contact site administrator for support.";
 SESSION_EXPIRE_MSG = "Your session is expired. Please login and try again.";
 //paging
