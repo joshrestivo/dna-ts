e6c9464e757ac4f4d0c19b920517ad5ef2b5f2d6
Thang <thangdx2901@gmail.com>
2016-11-02 16:41:40 +0700

    [admin] fix logout function.

--- admin/source/js/all.js
+++ admin/source/js/all.js
@@ -32,7 +32,7 @@ function getUrlParameter(sParam) {
 
 function logout() {
     $.ajax({
-		url: SERVICE_BASE_URL + '/logout',
+		url: SERVICE_BASE_URL + '/admin/logout',
 		xhrFields: {
 	      withCredentials: true
 	  	},
@@ -100,7 +100,6 @@ function deleteCookie(name) {
 
 // Check for authentication cookie
 var cookie = readCookie('TOWNSQUARE_ADMIN');
-console.log(cookie);
 var host = window.location.host;
 var current_url = window.location.href;
 
@@ -115,8 +114,8 @@ if (cookie == null){
 }
 
 // Define global variables
-SERVICE_BASE_URL = "http://localhost:9002/api/1.0";
-//SERVICE_BASE_URL = "https://townsquare-dev.herokuapp.com/api/1.0";
+//SERVICE_BASE_URL = "http://localhost:9002/api/1.0";
+SERVICE_BASE_URL = "https://townsquare-dev.herokuapp.com/api/1.0";
 SERVER_ERROR_MSG = "Server is error. Please contact site administrator for support.";
 SESSION_EXPIRE_MSG = "Your session is expired. Please login and try again.";
 //paging
