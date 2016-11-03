ef5fe703bd866f2d37262eacb4275317b95c187e
Cang Le <Cang-Le@Cang-Le-4.local>
2016-11-03 15:05:24 +0700

    [admin] - completed building page.

--- admin/source/js/admin/main/building/buildingController.js
+++ admin/source/js/admin/main/building/buildingController.js
@@ -1,15 +1,14 @@
 angular.module('app').controller('buildingController',  ['$scope', '$http','ngDialog', function ($scope, $http, ngDialog) {
 	$scope.Init = function(){
+		$scope.location_id=0;
     	$scope.currentPage = 1;
 	    $scope.itemPerPage = ITEM_PER_PAGE;
 	    $scope.maxSize = PAGE_MAX_SIZE;
 	    $http.get(SERVICE_BASE_URL+'/admin/locations ',{ withCredentials: true, headers: {'Access-Token': readCookie('TOWNSQUARE_ACCESS_TOKEN')} }).success(function (result) {
             if (result.success) {
             	$scope.locations = result.data;
-            	if( $scope.locations.length>0){
-            		$scope.location_id= $scope.locations[0].id;
-            	}
-            	$scope.TotalItems = $scope.locations.length;
+            	$scope.location_id=$scope.locations[0].id;
+            	getData($scope.location_id);
             }else {
 	                	if(result.data == "INVALID_SESSION") {                	
 	                		showErrorDialog(ngDialog,"Invalid login credantial");
@@ -20,17 +19,14 @@ angular.module('app').controller('buildingController',  ['$scope', '$http','ngDi
 	            })
 	            .error(function (error, status){
 	            	showErrorDialog(ngDialog,SERVER_ERROR_MSG);
-	  			}); 
-	  			if($scope.location_id){
-	  				getData($scope.location_id);			
-	  			}
-      };
-    
+	  			});
+	};
+  		
     var getData = function(location_id){
-    	$http.get(SERVICE_BASE_URL+'/admin/location/'+location_id+'/buidlings ',{ withCredentials: true, headers: {'Access-Token': readCookie('TOWNSQUARE_ACCESS_TOKEN')} }).success(function (result) {
+    	$http.get(SERVICE_BASE_URL+'/admin/location/'+location_id+'/buildings ',{ withCredentials: true, headers: {'Access-Token': readCookie('TOWNSQUARE_ACCESS_TOKEN')} }).success(function (result) {
             if (result.success) {
-            	$scope.buidlings = result.data;
-            	$scope.TotalItems = $scope.locations.length;
+            	$scope.buildings = result.data;
+            	$scope.TotalItems = $scope.buildings.length;
             }else {
 	                	if(result.data == "INVALID_SESSION") {                	
 	                		showErrorDialog(ngDialog,"Invalid login credantial");
@@ -45,7 +41,7 @@ angular.module('app').controller('buildingController',  ['$scope', '$http','ngDi
     };
     
     $scope.changeLocation=function(location_id){
-    	getData(locationId);
+    	getData(location_id);
     };
     
     $scope.addNewBuilding = function(){
@@ -64,7 +60,7 @@ angular.module('app').controller('buildingController',  ['$scope', '$http','ngDi
 	        data: '{"message":"Are you want to delete building: ' + building.name + '?"}'
 		})
 		.then(function (value) {
-           $http.get(SERVICE_BASE_URL+'/admin/location/'+building.location_id+'/building/'+buiding.id+'/del  ',{ withCredentials: true ,headers: {'Access-Token': readCookie('TOWNSQUARE_ACCESS_TOKEN')}}).success(function (result) {
+           $http.get(SERVICE_BASE_URL+'/admin/location/'+building.location_id+'/building/'+building.id+'/del  ',{ withCredentials: true ,headers: {'Access-Token': readCookie('TOWNSQUARE_ACCESS_TOKEN')}}).success(function (result) {
 			            if (result.success) {
 			            getData(building.location_id);
 			            }else {
